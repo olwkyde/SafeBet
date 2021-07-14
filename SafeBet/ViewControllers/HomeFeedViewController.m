003b1cec501198a3887ca782c971eee874711f1a
Isaac Oluwakuyide <ioluwakuyide@fb.com>
2021-07-14 15:50:54 -0400

    displayed cells from API call

--- SafeBet/ViewControllers/HomeFeedViewController.m
+++ SafeBet/ViewControllers/HomeFeedViewController.m
@@ -9,9 +9,14 @@
 #import <Parse/Parse.h>
 #import "SceneDelegate.h"
 #import "LoginViewController.h"
+#import "EventCell.h"
+#import "APIManager.h"
+#import "Events.h"
 
-@interface HomeFeedViewController ()
+@interface HomeFeedViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *logOutButton;
+@property (strong, nonatomic) NSMutableArray *arrayOfBets;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 
 
@@ -23,8 +28,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self fetchBets];
     
 }
+
+- (void) fetchBets  {
+    APIManager *api = [APIManager shared];
+    
+    [api fetchEventsWithCompletion:^(NSArray *bets, NSError *error)  {
+        if (error)  {
+            NSLog(@"Error fetching bets: %@", [error localizedDescription]);
+        }   else    {
+            self.arrayOfBets = bets;
+            [self.tableView reloadData];
+            NSLog(@"%@", bets);
+        }
+    }];
+}
+
 - (IBAction)logOutButton:(id)sender {
     [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
         SceneDelegate *myDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
@@ -44,5 +68,26 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    EventCell *cell = [tableView dequeueReusableCellWithIdentifier:@"EventCell"];
+    Events *event = self.arrayOfBets[indexPath.row];
+    
+    cell.event = event;
+    cell.dayLabel.text = event.date;
+    cell.timeLabel.text = event.time;
+    cell.team1Label.text = event.team1;
+    cell.team2Label.text = event.team2;
+    cell.team1OddsLabel.text = [NSString stringWithFormat:@"%d", event.team1Odds];
+    cell.team2OddsLabel.text = [NSString stringWithFormat:@"%d", event.team2Odds];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 7;
+}
+
 
 @end
